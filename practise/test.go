3bdeda44185ceda67cb6b6aea11ecc87c8a3a9b2
Amit yadav <yamit@iitk.ac.in>
2019-04-05 03:02:00 +0530

    Added full support for arrays, structures, sizes of vars

--- practise/test.go
+++ practise/test.go
@@ -1,62 +1,81 @@
-type new_int int;
-type new_new_int new_int;
-var glob new_new_int;
+// type new_int int;
+// type new_new_int new_int;
+// var glob new_new_int;
 var naya_structure struct {
-  a,b,c int
+  a,b,c int;
+  S struct{
+    p,q int;
+  };
+  arr [10]int;
 };
-func print();
-func main(a int, b string) int{
+type node struct{
+  a int;
+  b string;
+};
+var sme node;
+//
 
+
+
+// func print();
+func main(a int, b string) int{
     // const (
     //   c0 = 1;  // c0 == 0
     //   c1 = 2.0;  // c1 == 1
     //   c2, c3 string = "amit", "yadav";  // c2 == 2
     //   a,b int;
     // );
+    var arr [10] [10]int;
     // const c = 2 + 3;
     // Here's a basic example.
-    var e,f,g int = 1,2,3;
-    var m float = 1.3;
-    e = f+g;
-    e = 10;
-    e = 2 + 4;
-    // c = 10;
-    if 7%2 == 0 {
-        var a integer;
-        m = e;
-    } else {
-        var b charere;
-        f = e + g;
-    }
+    // var e,f,g int = 1+2,2,3;
+    // var m float = 1;
+    // var arr [4] int;
+    // // m = 2;
+    // var str string = "amit";
+    // var str string = "amit";
+    // str = 2;
+
+    // e = f+g;
+    // e = 10;
+    // e = 2 + 4;
+    // // c = 10;
+    // if 7%2 == 0 {
+    //     var a integer;
+    //     m = e;
+    // } else {
+    //     var b charere;
+    //     f = e + g;
+    // }
     // outerloop :
-    var i,j int;
-    for i = 1 ; i < 10 ; i = i + 1{
-      // break;
-      for j = i ; j < 20 ; j = j + 1{
-        if i%2 == 0 {
-            var a integer;
-            m = e;
-            // continue;
-        } else {
-            var b charere;
-            f = e + g;
-            // continue outerloop;
-        }
-        e = 2 + 4;
-        if j%2 == 0 {
-          f = e*g;
-          break;
-        }
-        if i+j % 25 == 0{
-          // break outerloop;
-        }
-      }
-    }
+    // var i,j int;
+    // for i = 1 ; i < 10 ; i = i + 1{
+    //   // break;
+    //   for j = i ; j < 20 ; j = j + 1{
+    //     if i%2 == 0 {
+    //         var a integer;
+    //         m = e;
+    //         // continue;
+    //     } else {
+    //         var b charere;
+    //         f = e + g;
+    //         // continue outerloop;
+    //     }
+    //     e = 2 + 4;
+    //     if j%2 == 0 {
+    //       f = e*g;
+    //       break;
+    //     }
+    //     if i+j % 25 == 0{
+    //       // break outerloop;
+    //     }
+    //   }
+    // }
     //
     // // You can have an `if` statement without an else.
     // if  8%4 == 0 {
-    //     const amit string;
-    // };
+    //     const amit string="hello";
+    // }
 
     // A statement can precede conditionals; any variables
     // declared in this statement are available in all
